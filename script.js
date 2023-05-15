62e9e1abb5ca6696a03e69c46bd43892434aa7e3
arinano <animeiris620@gmail.com>
2023-05-15 17:52:15 -0400

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+let mybutton = document.getElementById("button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+} 
